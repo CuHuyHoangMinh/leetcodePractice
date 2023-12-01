6e6558b3c1198f24364ea770c667712d5f0308fd
Huy Hoang Minh Cu <cuhuyhoangminh@outlook.com>
2023-11-30 20:22:11 -0500

    first commit

---
+++ Valid Palindrome/1073849341.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        String st = "";
+        s = s.toLowerCase();
+        for (int i = 0; i< s.length();i++){
+            char t = s.charAt(i);
+            if ((t<='z' && t >='a') || (t>='0' && t<='9')){
+                st = st + t;
+            }
+        }
+        if (st.length()<2) return true;
+        for (int i = 0;i<st.length();i++){
+            if (st.charAt(i)!=st.charAt(st.length()-i-1)) return false;
+        }
+    return true;
+    }
+}
