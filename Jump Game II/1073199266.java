68cd1a26c92b8c5917d0e4b9a4511c6fe9b19033
Huy Hoang Minh Cu <cuhuyhoangminh@outlook.com>
2023-11-30 20:21:00 -0500

    first commit

---
+++ Jump Game II/1073199266.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        if (n==1) return 0;
+        int max = 0 + nums[0];
+        int cap = nums[0];
+        int re = 1;
+        for (int i = 1;i<n-1;i++){
+            if (i+ nums[i]>max) {
+                max = i+ nums[i];
+            }
+            if (i == cap){
+                cap = max;
+                re++;
+            }
+        }
+        return re;
+        
+    }
+}
