92d506dc5fec7865fefc49dfca66f42a805af2bc
Huy Hoang Minh Cu <cuhuyhoangminh@outlook.com>
2023-11-30 20:21:15 -0500

    first commit

---
+++ Trapping Rain Water/1077167694.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int trap(int[] r) {
+        int n = r.length;
+        int max = 0;
+        int sum = 0;
+        int [] left = new int[n];
+        int [] right = new int[n];
+        for (int i=0;i<n;i++){
+            if (r[i]>max){
+                max = r[i];
+                left[i] = 0;
+            } else{
+                left[i] = max- r[i];
+            }
+        }
+        max = 0;
+        for (int i=n-1;i>=0;i--){
+            if (r[i]>max){
+                max = r[i];
+                right[i] = 0;
+            }
+            else{
+                right[i] = max-r[i];
+            }
+        }
+        for (int i = 0;i<n;i++){
+            sum = sum + Integer.min(left[i],right[i]);
+        }
+        
+        return sum;
+        
+    }
+}
