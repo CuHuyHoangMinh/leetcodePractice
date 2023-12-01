8352cce7cc099232cf039280a805ce302de1596e
Huy Hoang Minh Cu <cuhuyhoangminh@outlook.com>
2023-11-30 20:21:22 -0500

    first commit

---
+++ Reverse Words in a String/1077923764.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String reverseWords(String s) {
+        String [] temp = s.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i = temp.length-1; i>=0;i--){
+            // System.out.println(temp[i]);
+            sb.append(temp[i] + " ");
+        }
+        return sb.toString().trim();
+        
+    }
+}
